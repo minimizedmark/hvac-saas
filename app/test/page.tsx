0d981ac35888aaf3723e29744e79ddf99581cb04
MARK PARSON <anon4me785@gmail.com>
2025-11-13 21:21:50 -0700

    Create page.tsx
    test: add routing test page

---
+++ app/test/page.tsx
@@ -0,0 +1,13 @@
+export default function TestPage() {
+  return (
+    <div style={{ 
+      padding: '120px', 
+      fontSize: '48px', 
+      fontWeight: 'bold', 
+      textAlign: 'center',
+      color: '#1e40af'
+    }}>
+      TEST PAGE WORKS
+    </div>
+  );
+}
